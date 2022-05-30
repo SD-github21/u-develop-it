ee1e150c0ceb9f37c5be72f29b761855a94d6e92
SD-github21 <sapandonde.gmail.com>
2022-05-29 22:08:50 -0500

    set up initial server, middleware, and handling of 404 requests

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+// Import all modules/packages/ dependencies
 require('dotenv').config()
+const express = require("express");
+
+// Set up Express.js server
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express.js middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+})
+
+// Start the Express.js server on port 3001 and instruct it to listen for requests
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
